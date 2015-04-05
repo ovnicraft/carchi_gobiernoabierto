1af7ce1ca0a83176809349ff23660003c2f8e28a
Antonio Pardo <apardo@alabs.org>
2015-04-05 16:15:39 -0500

    add default env for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
