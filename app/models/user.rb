5fa52e493eea67b60c6764498c6b50c53cb74b84
Antonio Pardo <apardo@alabs.org>
2015-05-30 15:10:34 -0500

    fix nomethoderror

--- app/models/user.rb
+++ app/models/user.rb
@@ -499,6 +499,37 @@ class User < ActiveRecord::Base
     email_exception { notification.deliver }
   end
 
+  # Los usuario de este tipo no necesitan algunos de los campos de la tabla <tt>users</tt>.
+  # Aquí vaciamos esos campos.
+  def reset_unnecessary_fields
+    self.department_id = nil
+    self.media = nil
+    self.stream_flow_ids = []
+    self.media = nil
+    self.raw_location = nil
+    self.lat = nil
+    self.lng = nil
+    self.city = nil
+    self.state = nil
+    self.country_code = nil
+    self.zip = nil
+    self.photo_file_name = nil
+    self.photo_content_type = nil
+    self.photo_file_size = nil
+    self.photo_updated_at = nil
+    self.url = nil
+    self.organization = nil
+    self.public_role_es = nil
+    self.public_role_eu = nil
+    self.public_role_en = nil
+    self.gc_id = nil
+    self.description_es = nil
+    self.description_eu = nil
+    self.description_en = nil
+    self.politician_has_agenda = nil
+  end
+
+
   protected
     def exclusion_conditions(news_ids)
       conditions = news_ids.length > 0 ? "documents.id NOT IN (#{news_ids.join(',')})" : nil
@@ -581,33 +612,4 @@ class User < ActiveRecord::Base
       end while User.exists?(column => self[column])
     end
   
-    # Los usuario de este tipo no necesitan algunos de los campos de la tabla <tt>users</tt>.
-    # Aquí vaciamos esos campos.
-    def reset_unnecessary_fields
-      self.department_id = nil
-      self.media = nil
-      self.stream_flow_ids = []
-      self.media = nil
-      self.raw_location = nil
-      self.lat = nil
-      self.lng = nil
-      self.city = nil
-      self.state = nil
-      self.country_code = nil
-      self.zip = nil
-      self.photo_file_name = nil
-      self.photo_content_type = nil
-      self.photo_file_size = nil
-      self.photo_updated_at = nil
-      self.url = nil
-      self.organization = nil
-      self.public_role_es = nil
-      self.public_role_eu = nil
-      self.public_role_en = nil
-      self.gc_id = nil
-      self.description_es = nil
-      self.description_eu = nil
-      self.description_en = nil
-      self.politician_has_agenda = nil
-    end
 end
