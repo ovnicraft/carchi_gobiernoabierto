01c0dea68a34821882e9fdeb96dfb4ba823329f9
Antonio Pardo <apardo@alabs.org>
2015-06-27 05:36:43 -0500

    fix duplicate key

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -4,15 +4,14 @@ module Admin::AdminHelper
   def menu_link_class(tab)
     @current_tab.eql?(tab) ? "active" : "passive"
   end
-  
+
   def admin_section_name
     titles = {:menus => 'Misc', :depts => t('admin.menu.departamentos'), :users => t('admin.menu.usuarios'),
               :stats => t('admin.menu.estadisticas'), :news => t('admin.menu.noticias'),
-              :events => t('admin.menu.agenda'), :videos => t('admin.menu.web_tv'), 
+              :events => t('admin.menu.agenda'), :videos => t('admin.menu.web_tv'),
               :photos => t('admin.menu.fototeca'), :pages => t('admin.menu.paginas'),
               :links => t('admin.menu.enlaces'), :posts => 'Blog', :proposals => t('admin.menu.propuestas'),
-              :stream_flows => t('admin.menu.streaming'),
-              :users => 'Usuarios'
+              :stream_flows => t('admin.menu.streaming')
               }
      text = titles[@current_tab] || t("admin.menu.#{@current_tab}")
      text = nil if text.eql?(@title)
@@ -26,7 +25,7 @@ module Admin::AdminHelper
   def file_name_for_path(path)
     Pathname.new(path).basename
   end
-  
+
   def convert_relative_urls_to_absolute(text)
     e = "http://#{ActionMailer::Base.default_url_options[:host]}"
     # e = "#{request.protocol}#{request.host_with_port}"
