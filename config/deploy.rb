f17fafc97cea200d031bccb79f4694a9400b0a23
Andrés Pereira <apereira@alabs.es>
2015-09-17 07:36:10 -0500

    [deploy] fixing cap for db:schema:load vs db:migrate on init db

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,4 +15,46 @@ namespace :deploy do
     end
   end
   after :publishing, :restart
+
+  ## http://stackoverflow.com/a/1645590
+  #namespace :deploy do
+  #  task :cold do       # Overriding the default deploy:cold
+  #    update
+  #    load_schema       # My own step, replacing migrations.
+  #    start
+  #  end
+  #  task :load_schema, :roles => :app do
+  #    run "cd #{current_path}; rake db:schema:load"
+  #  end
+  #end
+  #
+end
+
+namespace :deploy do
+
+  task :cold do
+    invoke 'deploy:starting'
+    invoke 'deploy:started'
+    invoke 'deploy:updating'
+    invoke 'bundler:install'
+    invoke 'deploy:load_schema' # This replaces deploy:migrations
+    invoke 'deploy:compile_assets'
+    invoke 'deploy:normalize_assets'
+    invoke 'deploy:publishing'
+    invoke 'deploy:published'
+    invoke 'deploy:finishing'
+    invoke 'deploy:finished'
+  end
+
+  desc 'Load DB schema - CAUTION: rewrites database!'
+  task :load_schema do
+    on roles(:db) do 
+      within release_path do
+        with rails_env: (fetch(:rails_env) || fetch(:stage)) do
+          execute :rake, 'db:schema:load' # This creates the database tables AND seeds
+        end
+      end
+    end
+  end
+
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,3 @@
-server 'beta.gobiernoabierto.carchi.gob.ec', user: 'irekia', roles: %w{web app db}
-set :ssh_options, user: 'capistrano', forward_agent: true
+server 'beta.gobiernoabierto.carchi.gob.ec', roles: %w{web app db}
 set :branch, "staging"
 set :deploy_to, "/var/www/beta.gobiernoabierto.carchi.gob.ec"
